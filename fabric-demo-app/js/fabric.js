c655f79adfb54319bd6498f8368efe0f8061c001
Dan Rose <dan.rose@quadrotech-it.com>
2020-04-18 17:24:33 +0100

    Added new fabric omponents to test project

--- fabric-demo-app/js/fabric.js
+++ fabric-demo-app/js/fabric.js
@@ -1,13 +1,35 @@
-    var buttonElements = document.getElementById('test');
+console.log('fabric.js');
 
-    buttonElements.onclick = function() {
-        for (var i = 0; i < ButtonElements.length; i++) {
-            new fabric['Button'](ButtonElements[i], function () {
-                console.log('clicked');
-            });
-        }
-    }
+// button-click
+var ButtonElements = document.querySelectorAll(".ms-Button");
+for (var i = 0; i < ButtonElements.length; i++) {
+    new fabric['Button'](ButtonElements[i], function () {
+        console.log('clicked');
+        alert("You clicked the Create Account button");
+    });
+}
 
-    
-    // Try this instead
-    // object.addEventListener("click", myScript);
+// dropdown
+var DropdownHTMLElements = document.querySelectorAll('.ms-Dropdown');
+for (var i = 0; i < DropdownHTMLElements.length; ++i) {
+    var Dropdown = new fabric['Dropdown'](DropdownHTMLElements[i]);
+}
+
+//list
+var ListElements = document.querySelectorAll(".ms-List");
+for (var i = 0; i < ListElements.length; i++) {
+    new fabric['List'](ListElements[i]);
+}
+
+// left-navigation-bar
+var PanelExamples = document.getElementsByClassName("ms-PanelExample");
+for (var i = 0; i < PanelExamples.length; i++) {
+  (function() {
+    var PanelExampleButton = PanelExamples[i].querySelector(".ms-Button");
+    var PanelExamplePanel = PanelExamples[i].querySelector(".ms-Panel");
+    PanelExampleButton.addEventListener("click", function(i) {
+      new fabric['Panel'](PanelExamplePanel);
+    });
+  }());
+}
+
