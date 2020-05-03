281e3fdf249593c79f6fa45cba45ffadea1e0d8d
Dan Rose <dan.rose@quadrotech-it.com>
2020-05-03 18:15:26 +0100

    update to my-learning-stack

--- my-learning-stack/js/main.js
+++ my-learning-stack/js/main.js
@@ -1,7 +1,21 @@
 const progress = document.querySelectorAll('.progress-done');
+var value1 = 12;
+var value2 = 47;
+var value3 = 33;
+var value4 = 87;
+var value5 = 3;
+var value6 = 29;
+
+// var i;
+
+// for (i = 0; i < progress.length; i++) {
+//     progress[i].style.width = progress[i].getAttribute('data-done') + '%';
+//     progress[i].style.opacity = 1;
+//   }
 
 var i;
 for (i = 0; i < progress.length; i++) {
-    progress[i].style.width = progress[i].getAttribute('data-done') + '%';
-    progress[i].style.opacity = 1;
-  }
+    document.getElementById("value" + 3).innerHTML = value3 + "%";
+    //progress[i].style.width = progress[i].getAttribute('value3') + '%';
+    //progress[i].style.opacity = 1;
+ }
